d0ccc7699e655ddf8a1e747bd12a67dc14ae164e
aliengll7520-maker <aliengll7520@gmail.com>
2026-08-07 20:48:05 +0700

    Update Header.jsx

--- labor-platform/frontend/website/src/components/Header/Header.jsx
+++ labor-platform/frontend/website/src/components/Header/Header.jsx
@@ -1,23 +1,11 @@
 import React from "react";
+import "./Header.css";
 
 function Header({
   title = "Labor Supply Platform",
 }) {
   return (
-    <header
-      style={{
-        width: "100%",
-        height: "60px",
-        backgroundColor: "#1976d2",
-        color: "#ffffff",
-        display: "flex",
-        alignItems: "center",
-        padding: "0 20px",
-        boxSizing: "border-box",
-        fontSize: "20px",
-        fontWeight: "600",
-      }}
-    >
+    <header className="header">
       {title}
     </header>
   );
